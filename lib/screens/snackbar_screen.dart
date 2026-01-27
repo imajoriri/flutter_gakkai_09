4713eb30ed7976052326ba553d9d4e07ea398315
imajoriri <imajo.srroni@gmail.com>
2026-01-27 10:32:05 +0900

    Refactor SnackbarScreen layout to use a Column for better organization; separate buttons for single and double tap actions with distinct labels for clarity.

--- lib/screens/snackbar_screen.dart
+++ lib/screens/snackbar_screen.dart
@@ -10,23 +10,39 @@ class SnackbarScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(backgroundColor: theme.colorScheme.inversePrimary),
       body: Center(
-        child: GestureDetector(
-          onDoubleTap: () {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: const Text('ダブルタップ'),
-                backgroundColor: Colors.blue,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          spacing: 16,
+          children: [
+            GestureDetector(
+              child: ElevatedButton(
+                onPressed: () {
+                  ScaffoldMessenger.of(
+                    context,
+                  ).showSnackBar(const SnackBar(content: Text('シングルタップ')));
+                },
+                child: const Text('onTapのみ!!'),
               ),
-            );
-          },
-          child: ElevatedButton(
-            onPressed: () {
-              ScaffoldMessenger.of(
-                context,
-              ).showSnackBar(const SnackBar(content: Text('シングルタップ')));
-            },
-            child: const Text('Tap!!'),
-          ),
+            ),
+            GestureDetector(
+              onDoubleTap: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: const Text('ダブルタップ'),
+                    backgroundColor: Colors.blue,
+                  ),
+                );
+              },
+              child: ElevatedButton(
+                onPressed: () {
+                  ScaffoldMessenger.of(
+                    context,
+                  ).showSnackBar(const SnackBar(content: Text('シングルタップ')));
+                },
+                child: const Text('onTapとonDoubleTap!!'),
+              ),
+            ),
+          ],
         ),
       ),
     );
